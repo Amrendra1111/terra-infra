32944526f536c19c68b3d767eb85df77f9e89f4e
Amrendra1111 <amrendra1111singh@gmail.com>
2024-08-25 13:05:03 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+# Specify the provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Define an EC2 instance resource
+resource "aws_instance" "example" {
+  ami           = "ami-025fe52e1f2dc5044" # Amazon Linux 2 AMI (you can change this)
+  instance_type = "t2.micro"
+
+  # Add tags to the instance
+  tags = {
+    Name = "Terraform Example"
+  }
+}
